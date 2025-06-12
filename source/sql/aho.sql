4f877714f6518f7204225396d342fe668ee49f3c
plusdojo2025fukumoto <fukumoto-kouki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 17:02:43 +0900

    SQL作成

---
+++ source/sql/aho.sql
@@ -0,0 +1,32 @@
+create table WANKO (
+		dogName varchar(50) NOT NULL,
+		dogBreed varchar(50) NOT NULL,
+		dogBirth date NOT NULL,
+		gender boolean NOT NULL,
+		state varchar(300) NOT NULL,
+		wakuchin varchar(300) NOT NULL,
+		dogId int NOT NULL AUTO_INCREMENT,
+		FOREIGN KEY (nameId) varchar(50) NOT NULL ,
+		dogPhoto varchar(300),
+		kyosei boolean NOT NULL,
+		dogRegist date,
+		remarks1 varchar(300),
+		remarks2 varchar(300),
+		remarks3 varchar(300),
+		remarks4 varchar(300),
+		remarks5 varchar(300),
+		injection varchar(300),
+		rabies varchar(300)
+		);
+		
+		
+create table SCHOOL (
+		schoolId int PRIMARY KEY
+);
+
+
+
+
+
+
+		

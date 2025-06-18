432092f8baf4ac8cac9c1440b28b4892c0f8d08c
plusdojo2025-morita <morita-so-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 14:02:03 +0900

    Update AllDto.java

--- source/src/main/java/dto/AllDto.java
+++ source/src/main/java/dto/AllDto.java
@@ -56,7 +56,7 @@ public class AllDto implements Serializable {
 	 
 	 //カレンダー
 	 private int calendarId;			/*カレンダーID*/
-	 private LocalDate calendarDate;			/*日付*/
+	 private LocalDateTime calenderDate;			/*日付*/
 	 private String title;	/*タイトル*/
 	 private LocalTime time;			/*時間*/
 	 private int calendarMemo;				/*メモ*/
@@ -70,7 +70,7 @@ public class AllDto implements Serializable {
 	 private String training;				/*トレーニング*/
 	 private String reportMemo;			/*メモ*/
 	 private LocalDate reportDate;		/*日付*/
-	 private int DogId;			/*ワンコID*/
+	 private int reportDogId;			/*ワンコID*/
 	 
 	//ゲッターセッターの生成
 	 //ワンコテーブル用ゲッターセッターの生成
@@ -357,7 +357,7 @@ public class AllDto implements Serializable {
 		this.hardness = hardness;
 	}
 
-	public boolean isAbnormal() {
+	public boolean getAbnormal() {
 		return abnormal;
 	}
 
@@ -399,12 +399,12 @@ public class AllDto implements Serializable {
 		this.calendarId = calendarId;
 	}
 
-	public LocalDate getCalendarDate() {
-		return calendarDate;
+	public LocalDateTime getCalenderDate() {
+		return calenderDate;
 	}
 
-	public void setCalendarDate(LocalDate calendarDate) {
-		this.calendarDate = calendarDate;
+	public void setCalenderDate(LocalDateTime calenderDate) {
+		this.calenderDate = calenderDate;
 	}
 
 	public String getTitle() {
@@ -498,12 +498,12 @@ public class AllDto implements Serializable {
 		this.reportDate = reportDate;
 	}
 
-	public int getDogId() {
-		return DogId;
+	public int getReportDogId() {
+		return reportDogId;
 	}
 
-	public void setDogId(int DogId) {
-		this.DogId = DogId;
+	public void setReportDogId(int reportDogId) {
+		this.reportDogId = reportDogId;
 	}
 	
 

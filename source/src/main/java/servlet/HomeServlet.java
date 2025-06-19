e45c6fb63077982af24a62f150641ebb8a4bb9e0
plusdojo2025-morita <morita-so-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 13:03:25 +0900

    Update HomeServlet.java

--- source/src/main/java/servlet/HomeServlet.java
+++ source/src/main/java/servlet/HomeServlet.java
@@ -2,35 +2,54 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.CalendarDAO;
 
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet(urlPatterns = {"/", "/HomeServlet"})
+@WebServlet(urlPatterns = "/HomeServlet")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/webapp/LoginServlet");
+			return;
+		}
+		
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String calendarId = request.getParameter("calendarId");
+		String calendarDate = request.getParameter("calendarDate");
+		String title = request.getParameter("title");
+		String time = request.getParameter("time");
+		String calendarMemo = request.getParameter("calendarMemo");
+		String calendarDogId = request.getParameter("calendarDogId");
+		
+		CalendarDAO cDao = new CalendarDAO();
+		
+		//List<AllDto> calendar = cDao.select(new AllDto(calendarId, calendarDate, title, time, calendarMemo, calendarDogId));
+		
+		// メニューページにフォワードする
+		if(session.getAttribute("userUniqueId") == Boolean.TRUE) {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/t_home.jsp");
+		dispatcher.forward(request, response);
+		}else if(session.getAttribute("userUniqueId") == Boolean.FALSE){
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/o_home.jsp");
+			dispatcher.forward(request, response);	
+		}
+		}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */

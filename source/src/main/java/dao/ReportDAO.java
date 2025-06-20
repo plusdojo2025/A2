909efd32acbe9b5bd7d8a7608b0aa42d7a0f8ef8
Shimosaka02 <shimosaka-arisa-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 14:28:08 +0900

    Update ReportDAO.java

--- source/src/main/java/dao/ReportDAO.java
+++ source/src/main/java/dao/ReportDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dto.AllDto;
+import dto.Bc;
 
 
 
@@ -248,6 +249,85 @@ public class ReportDAO {
 
 			// 結果を返す
 			return result;
-			}
+	
 		}
+
+		// 引数report指定された項目で検索して、取得されたデータのリストを返す
+		public List<AllDto> select(AllDto report) {
+			Connection conn = null;
+			List<Bc> cardList = new ArrayList<Bc>();
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("com.mysql.cj.jdbc.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webapp2?"
+						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+						"root", "password");
+
+				// SQL文を準備する
+				String sql = "SELECT number, company, department, position, name, zipcode, address, phone, fax, email, remarks FROM Bc WHERE company LIKE ? AND name LIKE ? AND address LIKE ? ORDER BY number";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				if (card.getCompany() != null) {
+					pStmt.setString(1, "%" + card.getCompany() + "%");
+				} else {
+					pStmt.setString(1, "%");
+				}
+//				if (card.getName() != null) {
+//					pStmt.setString(2, "%" + card.getName() + "%");
+//				} else {
+//					pStmt.setString(2, "%");
+//				}
+//				if (card.getAddress() != null) {
+//					pStmt.setString(3, "%" + card.getAddress() + "%");
+//				} else {
+//					pStmt.setString(3, "%");
+//				}
+				
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) {
+					Bc bc = new Bc(rs.getInt("number"),
+								   rs.getString("company"),
+								   rs.getString("department"),
+								   rs.getString("position"),
+								   rs.getString("name"),
+								   rs.getString("zipcode"),
+								   rs.getString("address"),
+								   rs.getString("phone"),
+								   rs.getString("fax"),
+								   rs.getString("email"),
+								   rs.getString("remarks"));
+					// bc.setNumber(rs.getInt("number"));とかでも書ける
+					cardList.add(bc);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				cardList = null;
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				cardList = null;
+			} finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+						cardList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return cardList;
+		}
+
+}
 

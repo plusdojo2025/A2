808c67c8c08bcfb5deccd209d96e4e52aa917265
plusdojo2025asada <asada-ririka-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 10:14:45 +0900

    poop

--- source/src/main/java/servlet/CalendarServlet.java
+++ source/src/main/java/servlet/CalendarServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,23 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import dto.AllDto;
-import dao.CalendarDAO;
-
 /**
  * Servlet implementation class CalendarServlet
  */
 @WebServlet("/CalendarServlet")
 public class CalendarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CalendarServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -34,19 +25,19 @@ public class CalendarServlet extends HttpServlet {
 		if (session.getAttribute("id") == null) {
 			response.sendRedirect("/A2/LoginServlet");
 			return;
+		}else {
+		    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/schedule-regi.jsp");
+		    dispatcher.forward(request, response);
 		}
 		
 		//最初のアクション（予定登録を表示）
 		String action = request.getParameter("action");
-		if(action == null )
+		if(action == null ) {
+			
+		}
 	}
 	
 	
-
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);

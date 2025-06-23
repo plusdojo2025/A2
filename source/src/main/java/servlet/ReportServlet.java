0a22a5c58b0bb343e305601a09c55bc2b54cc7b6
plusdojo2025fukumoto <fukumoto-kouki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-23 09:56:45 +0900

    Update ReportServlet.java

--- source/src/main/java/servlet/ReportServlet.java
+++ source/src/main/java/servlet/ReportServlet.java
@@ -50,7 +50,7 @@ public class ReportServlet extends HttpServlet {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
-			response.sendRedirect("A2/LoginServlet");
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
 			return;
 		}
 
